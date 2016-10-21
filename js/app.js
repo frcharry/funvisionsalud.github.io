2842b051c48a84d2ded7991dcbfb90d0caa270e4
Fabio Charry <fabio.charry@gmail.com>
2016-10-21 15:06:21 -0500

    Actualización modelDialog
    Función modelDialog para las categorías de los servicios.

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     var target = this.hash;
     $target = $(target);
     $('html, body').stop().animate({
-      'scrollTop': $target.offset().top+2
+      'scrollTop': $target.offset().top+1
     }, 500, 'swing', function () {
       window.location.hash = target;
       $(document).on("scroll", onScroll);
