505b274676d268d5a7631170b7ad17d607664fb8
Fabio Charry <fabio.charry@gmail.com>
2017-03-24 16:21:43 -0500

    Revert "Ajustes de logo"
    This reverts commit 626e0dcea901ef92342c406703c138bf7eaecd2c.

--- js/myjquery.js
+++ js/myjquery.js
@@ -41,24 +41,6 @@ $(document).ready(function(){
 	$(document).on('click','.navbar-collapse.in',function(e) {
     if( $(e.target).is('a') ) {
         $(this).collapse('hide');
-    	}
-	});
-	$('#Carouselexp').carousel({
-  		interval: 4000
-		});
-
-	$('.carousel .item').each(function(){
-  			var next = $(this).next();
-  		if (!next.length) {
-    		next = $(this).siblings(':first');
-  		}
-  		next.children(':first-child').clone().appendTo($(this));
-  		for (var i=0;i<2;i++) {
-    		next=next.next();
-    		if (!next.length) {
-      			next = $(this).siblings(':first');
-    		}
-    	next.children(':first-child').clone().appendTo($(this));
-		}
-	});
+    }
+});
 });
